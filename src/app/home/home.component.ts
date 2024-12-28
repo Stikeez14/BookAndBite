9fe1c431696902e0432dfa7d1ec3691533573352
Braje Luca Ianis <jelucabra11@gmail.com>
2024-12-28 11:41:38 +0200

    Added a personalized welcome

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,13 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { getAuth, onAuthStateChanged, User } from 'firebase/auth';
 
 @Component({
   selector: 'app-home',
-  imports: [],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
-  constructor() {
-  }
+export class HomeComponent implements OnInit {
+  userEmail: string | null = null;
+  username: string | null = null;
 
+  constructor() {}
+
+  ngOnInit(): void {
+    const auth = getAuth();
+    onAuthStateChanged(auth, (user: User | null) => {
+      if (user) {
+        // User is signed in
+        this.userEmail = user.email;
+        // @ts-ignore
+        this.username = this.extractEmailUsername(user.email);
+      } else {
+        // User is not signed in
+        this.userEmail = null;
+      }
+    });
+  }
+  extractEmailUsername(email: string): string {
+
+    const username = email.split('@')[0]; // Extract the username part
+    const firstCharUppercase = username.charAt(0).toUpperCase(); // Convert the first character to uppercase
+    const remainingChars = username.slice(1); // Extract the rest of the username
+    return firstCharUppercase + remainingChars;  }
 }
+
